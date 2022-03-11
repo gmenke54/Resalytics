0784da6d6b12877724a32655315b279a7d3faf6d
gmenke54 <menke.gw@gmail.com>
2022-03-10 16:09:19 -0800

    imported keywords.json; console.logs keywords and desc properly but in two different consoles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 /*global chrome*/
 import './App.css';
+import React, { useState } from 'react';
 import axios from 'axios';
+import keywords from './keywords-object.json';
 
 function App() {
+  const [desc, setDesc] = useState(null);
   const handleFileUpload = async (e) => {
     const formData = new FormData();
     const file = this.files[0];
@@ -27,11 +30,17 @@ function App() {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       function: () => {
-        console.log('running');
+        // this logs to the windows console:
         desc = window.getSelection().toString();
         console.log(desc);
+        // this throws error "Uncaught ReferenceError: f is not defined":
+        console.log(keywords);
       }
     });
+    // this logs to the extensions console:
+    console.log(keywords);
+    // this returns blank string from line 28:
+    console.log(desc);
   };
 
   return (
